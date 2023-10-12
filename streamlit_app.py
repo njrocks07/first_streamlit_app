a5a043e8895a47db4571e2a02693ad5739165483
njrocks07 <147590035+njrocks07@users.noreply.github.com>
2023-10-12 18:25:10 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,5 @@ streamlit.dataframe(my_data_rows)
 
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', fruit_choice)
